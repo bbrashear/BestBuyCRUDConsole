865cc8f85cc9aeb502dc2cf28bdc9e2023efd93e
Blake Brashear <bbrashear94@gmail.com>
2020-05-04 13:21:12 -0400

    Add project files.

---
+++ BestBuyBestPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestBuyBestPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
